aa42819a761baa8bd245e81e47551ba046a1fd2c
Carlos Escalona <ing.carlosuc@gmail.com>
2020-06-12 11:22:38 -0300

    StopWatch Exercice

--- Exercices_Section_2/StopWatch/StopWatch/Program.cs
+++ Exercices_Section_2/StopWatch/StopWatch/Program.cs
@@ -2,11 +2,34 @@
 
 namespace StopWatch
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var stopWatch = new StopWatch();
+            string userInput;
+
+            while (true)
+            {
+                Console.Write("Inserte instruccion:");
+                userInput = Console.ReadLine();
+                StopWatchInterface(userInput, stopWatch);
+            }
+        }
+
+        public static void StopWatchInterface(string userInput, StopWatch stopWatch)
+        {
+            switch (userInput.ToUpper())
+            {   case "START" :
+                    stopWatch.Start();
+                    break;
+                case "STOP":
+                    stopWatch.Stop();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
---
+++ Exercices_Section_2/StopWatch/StopWatch/StopWatch.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StopWatch
+{
+    class StopWatch
+    {
+
+        public TimeSpan CurrentTime { get; set; }
+        public bool IsCounting { get; set; }
+        private readonly TimeSpan _timeStep = TimeSpan.FromSeconds(1);
+        private System.Threading.Timer timer;
+
+
+        public StopWatch()
+        {
+            this.CurrentTime = new TimeSpan();
+            IsCounting = false;
+        }
+
+        public void Start()
+        {
+            
+
+            if(IsCounting == true)
+                    throw new InvalidOperationException("Error");
+
+            Console.WriteLine("Cuenta iniciada");
+            timer = new System.Threading.Timer(
+            e => Count(),
+            null,
+            TimeSpan.Zero,
+            TimeSpan.FromSeconds(1));
+
+            IsCounting = true;
+        }
+
+        public void Stop()
+        {
+            timer.Dispose();
+            IsCounting = false;
+            Console.WriteLine(CurrentTime);
+        }
+
+        private void Count()
+        {
+            CurrentTime = CurrentTime.Add(_timeStep);
+        }
+
+    }
+}
